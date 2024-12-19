b37d036e9d5bbab8db1bfc69a23aa8ebda96a263
JettWhi <gkdms0601@naver.com>
2024-12-19 13:50:31 +0900

    pm2:00 progress

--- app/src/main/java/com/ffarm/myapplication/ProfileActivity.java
+++ app/src/main/java/com/ffarm/myapplication/ProfileActivity.java
@@ -3,26 +3,26 @@ package com.ffarm.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class start_page_activity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_page); // 첫 번째 화면 레이아웃 설정
+        setContentView(R.layout.profile); // 첫 번째 화면 레이아웃 설정
 
-        ImageButton startButton = findViewById(R.id.Imagebutton2);
+        ImageButton startButton = findViewById(R.id.ImageButton2);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // ProfileActivity로 이동
-                Intent intent = new Intent(start_page_activity.this, ProfileActivity.class);
+                // DiaryActivity 이동
+                Intent intent = new Intent(ProfileActivity.this, DiaryActivity.class);
                 startActivity(intent);
                 finish(); // WelcomeActivity 종료
             }
         });
     }
+
 }
