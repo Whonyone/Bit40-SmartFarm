e35b652c73b3bb25c53a0754087cb20b3590e9f9
JettWhi <gkdms0601@naver.com>
2024-12-20 16:38:12 +0900

    layout integrate complete

--- app/src/main/java/com/ffarm/myapplication/start_page_activity.java
+++ app/src/main/java/com/ffarm/myapplication/start_page_activity.java
@@ -2,6 +2,7 @@ package com.ffarm.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -23,6 +24,10 @@ public class start_page_activity extends AppCompatActivity {
                 startActivity(intent);
                 finish(); // WelcomeActivity 종료
             }
+
         });
     }
 }
+
+
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "My SmartFarm"
 include(":app")
- 
